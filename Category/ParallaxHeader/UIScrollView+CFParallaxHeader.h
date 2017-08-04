dacfe2ec878483f24815ce608aaa723e6c20f3b6
Ken <coofree@foxmail.com>
2017-08-04 17:03:02 +0800

    ParallaxHeader

---
+++ Category/ParallaxHeader/UIScrollView+CFParallaxHeader.h
@@ -0,0 +1,19 @@
+//
+//  UIScrollView+CFParallaxHeader.h
+//  CFCustomTableView
+//
+//  Created by YF on 2017/7/10.
+//  Copyright © 2017年 CooFree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (CFParallaxHeader)
+
+@property (nonatomic, weak) UIView *topView;
+@property (nonatomic) CGFloat parallaxHeight;
+
+- (void)addParallaxHeadView:(UIView *)view;
+- (void)addParallaxHeadView:(UIView *)view isTranslucent:(BOOL)translucent;
+
+@end
